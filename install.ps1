680fa5a7228732e36ec296779b104d6ddd2be713
Leo Sun <leosunsl@outlook.com>
2021-12-28 07:00:14 +0800

    fix wrong scoop bucket name

--- install.ps1
+++ install.ps1
@@ -91,7 +91,7 @@ if (-not (Get-Command -Name scoop -ErrorAction SilentlyContinue)) {
     refreshenv
 }
 
-scoop bucket add extra
+scoop bucket add extras
 # bucket for chezmoi
 scoop bucket add twpayne https://github.com/twpayne/scoop-bucket
 
