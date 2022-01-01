b06b57b5cd6da47d58f0bd7177d53fd30758c2aa
leosun <leosunsl@outlook.com>
2022-01-01 11:57:37 +0800

    add vim-plug install at install.ps1

--- install.ps1
+++ install.ps1
@@ -120,4 +120,7 @@ winget install -e --id Git.Git
 
 # Initialize Chezmoi
 chezmoi init --apply breakersun
-chezmoi diff
+
+# install vim-plug
+iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+    ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
